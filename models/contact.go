eadf67f00ef4a5f3b703886091adcd65ffaa6da4
codetesla51 <uoladele99@gmail.com>
2025-09-16 09:20:31 +0100

    refactor and added new functions

--- models/contact.go
+++ models/contact.go
@@ -1,36 +1,36 @@
 package models
 
 import (
-    "context"
-    "errors"
-"time"
-    "github.com/codetesla51/portBackend/config"
+	"context"
+	"errors"
+	"github.com/codetesla51/portBackend/config"
+	"time"
 )
 
 type MessageInput struct {
-    Name    string `json:"name" binding:"required"`
-    Email   string `json:"email" binding:"required,email"`
-    Inquiry string `json:"inquiry" binding:"required"`
-    Message string `json:"message" binding:"required"`
+	Name    string `json:"name" binding:"required"`
+	Email   string `json:"email" binding:"required,email"`
+	Inquiry string `json:"inquiry" binding:"required"`
+	Message string `json:"message" binding:"required"`
 }
 
 func StoreMessage(input MessageInput) error {
-    query := `
+	query := `
     INSERT INTO contacts (name, email, inquiry, message, created_at)
     VALUES ($1, $2, $3, $4, $5)
 `
-_, err := config.DB.Exec(
-    context.Background(),
-    query,
-    input.Name,
-    input.Email,
-    input.Inquiry,
-    input.Message,
-    time.Now(),
-)
-    if err != nil {
-        return errors.New("message not sent: " + err.Error())
-    }
+	_, err := config.DB.Exec(
+		context.Background(),
+		query,
+		input.Name,
+		input.Email,
+		input.Inquiry,
+		input.Message,
+		time.Now(),
+	)
+	if err != nil {
+		return errors.New("message not sent: " + err.Error())
+	}
 
-    return nil
+	return nil
 }
