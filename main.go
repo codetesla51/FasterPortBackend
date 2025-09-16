73f35b2b457c480facd8075ffda32120241001fe
codetesla51 <uoladele99@gmail.com>
2025-09-16 09:44:35 +0100

    refactor and added new functions

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"https://your-frontend-domain.com", "http://localhost:3000"},
+		AllowOrigins:     []string{"https://devuthman.vercel.app/"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
 		AllowCredentials: true,
